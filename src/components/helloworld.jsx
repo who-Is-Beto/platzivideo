77162db98964e08e72f3690ccc5ce36f45ddb625
Beto Cortés <roberto.cortes13233@gmail.com>
2020-12-19 14:10:11 -0600

    eslint addes

--- src/components/helloworld.jsx
+++ src/components/helloworld.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import '../assets/styles/App.scss';
 
 export default function HelloWolrd({ hello }) {
   return (
     <h1>{hello}</h1>
-  )
-}
+  );
+};
