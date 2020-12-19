c8a5ca25e719050c43b593b4e1e66f4779de163a
Beto Cortés <roberto.cortes13233@gmail.com>
2020-12-19 13:26:27 -0600

    initial commit

---
+++ src/components/helloworld.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HelloWolrd({ hello }) {
+  return (
+    <h1>{hello}</h1>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import HelloWorld from './components/helloworld'
+
+ReactDOM.render(<HelloWorld hello='Hola mundo cabrones' />, document.getElementById('app'))
